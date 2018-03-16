c55405f024bea53d4c78b5e557e1fd3a7620c8e3
Patrick Archer <p.archer159@hotmail.com>
2018-03-16 13:46:25 -0700

    Completed - Analyzer v1.0
    Only known bug is if there are spaces @ the ends of any strings.  This should not be an issue, however, because the formatter should find and remove them ahead of time.

--- TextFormatter5000EclipseProject/src/textformatter/Analyzer.java
+++ TextFormatter5000EclipseProject/src/textformatter/Analyzer.java
@@ -8,19 +8,24 @@ import java.lang.*;
 
 public class Analyzer
 {
-	private static int wordCount = 0;
-	private static int lineCount = 0;
-	private static int blankLinesRemoved = 0;
-	private static float avgWordsPerLine = 0;
-	private static float avgLineLength = 0;
+    private static int inputLineCount = 0;
 
-	private static String passedLine;   // used for testing purposes; remove
+	private static int wordCount = 0;
+	private static int outputLineCount = 0;
+	private static int blankLinesRemoved = 0;
+	private static int totalLineLength = 0;
+
+	private static double avgWordsPerLine = 0;
+	private static double avgLineLength = 0;
+
+    private static String inputLine;    // temp var to store current input String[] line
+	private static String outputLine;   // temp var to store current output String[] line
 	
 	public int getWordCount() {return wordCount;}
-	public int getLineCount() {return lineCount;}
+	public int getLineCount() {return outputLineCount;}
 	public int getBlankLinesRemoved() {return blankLinesRemoved;}
-	public float getAvgWordsPerLine() {return avgWordsPerLine;}
-	public float getAvgLineLength() {return avgLineLength;}
+	public double getAvgWordsPerLine() {return avgWordsPerLine;}
+	public double getAvgLineLength() {return avgLineLength;}
 	
 	// inputLines is an array of the original, unformatted text.  This is only needed to figure out how many blank
 	// lines were removed.
@@ -33,42 +38,43 @@ public class Analyzer
         //*************************************************
 		for (int i = 0; i < outputLines.length && outputLines[i] != null; i++)  // "For each full string, do..."
         {
-            lineCount++;    // used in lineCount function
+            outputLineCount++;    // used in lineCount function
 
-            passedLine = outputLines[i];
+            // inputLine = inputLines[i];
+            outputLine = outputLines[i];
             boolean atFirstCell = true;     // used to determine if analytic "pointer" has moved off first char cell or not
 
             wordCount++;    // initially assume a word is being passed; in special cases, decrement wordCount if no words, etc.
             // Due to the way the method is set up, this increment is meant to account for losing the count on
             // the last word in the string.  If possible, adjust code so this is taken care of in an if statement, below.
 
-            if (passedLine == null)	// error check for totally null entries passed
+            if (outputLine == null)	// error check for totally null entries passed
 			{
                 wordCount = wordCount - 1;
                 // since wordCount is initially incremented, this undoes that, because there are no words in this case
 			}
-			else if (passedLine.equals(' ') || passedLine.equals(" ") || passedLine.equals("  "))
+			else if (outputLine.equals(' ') || outputLine.equals(" ") || outputLine.equals("  "))
 				// ** error catch for if entire string is simply a single space char; alter so doesn't only catch 1 or 2 space
 				// chars, but rather up to 80 of them **
+
+			    //********************************
+			    // Do not need to worry about at this time, as this case is taken care of in the formatter.
+			    //********************************
 			{
-				//*****************
-				// *** ?Delete warning message? ***
-				//*****************
-				System.out.println("\nWARNING: One or more strings provided does not contain any text" +
-                                " and is either blank or consists of only space characters.\n");
-				wordCount = wordCount - 1;	// do not change the count; same reason as for "if (passedLine == null)" case
+				// One or more strings provided does not contain any text and is either blank or consists of only space characters.
+				wordCount = wordCount - 1;	// do not change the count; same reason as for "if (outputLine == null)" case
 			}
 			else    // else, valid input string; good to go
 			{
                 atFirstCell = true;     // used to determine if moved off first char cell or not
 
-				for (int j = 0; j < passedLine.length(); j++) // "For each char in the single string, do..."
+				for (int j = 0; j < outputLine.length(); j++) // "For each char in the single string, do..."
 				{
-				    if (Character.isSpaceChar(passedLine.charAt(j)))   // char in string is a space
+				    if (Character.isSpaceChar(outputLine.charAt(j)))   // char in string is a space
                     {
                         if (atFirstCell == true)   // if space @ beginning of string, do...
                         {
-                            while (passedLine.charAt(j) == ' ' && j < passedLine.length())
+                            while (outputLine.charAt(j) == ' ' && j < outputLine.length())
                             // parse thru consecutive space chars until letter, number, or symbol found; cell location saved to var < j >
                             {
                                 j++;
@@ -77,10 +83,10 @@ public class Analyzer
                             wordCount++;    // a word has been found
                             atFirstCell = false;    // we aren't at the first cell anymore (since/after j is incremented)
                         }
-                        else if (atFirstCell == false && Character.isDefined(passedLine.charAt(j+1)))
+                        else if (atFirstCell == false && Character.isDefined(outputLine.charAt(j+1)))
                         // space is somewhere in middle of string, but not at beginning or end; we have found a word
                         {
-                            while (passedLine.charAt(j) == ' ' && j < passedLine.length())
+                            while (outputLine.charAt(j) == ' ' && j < outputLine.length())
                             // parse thru consecutive space chars until letter, number, or symbol found; cell location saved to var < j >
                             {
                                 j++;
@@ -90,7 +96,7 @@ public class Analyzer
                         }
                         else    // space is @ the very end of the string (or multiple spaces are)
                         {
-                            while (passedLine.charAt(j) == ' ' && j < passedLine.length())
+                            while (outputLine.charAt(j) == ' ' && j < outputLine.length())
                             // parse thru consecutive space chars until letter, number, or symbol found; cell location saved to var < j >
                             {
                                 j++;
@@ -115,22 +121,31 @@ public class Analyzer
         //*************************************************
         /*
         Line count is dealt with in the above function.  A counter is simply incremented each time a new line
-        is starting to be analyzed.  The public variable "lineCount" is updated each time.
+        is starting to be analyzed.  The public variable "outputLineCount" is updated each time.
          */
 
         //*************************************************
         /*  Deal with blankLinesRemoved */
         //*************************************************
-        // ...
+        for (int f = 0; f < inputLines.length && inputLines[f] != null; f++)
+        {
+            inputLineCount++;
+        }
+
+        blankLinesRemoved = inputLineCount - outputLineCount;   // delta( outputLineCount , inputLineCount ) = numLinesRemoved
 
         //*************************************************
         /*  Deal with avgWordsPerLine   */
         //*************************************************
-        // ...
+        avgWordsPerLine = wordCount / outputLineCount;
 
         //*************************************************
         /*  Deal with avgLineLength */
         //*************************************************
-        // ...
+        for (int k = 0; k < outputLines.length && outputLines[k] != null; k++)  // "For each full string, do..."
+        {
+            totalLineLength = outputLines[k].length() + totalLineLength;    // take string length of each string and update var
+        }
+        avgLineLength = (double) totalLineLength / (double) outputLineCount;    // calculate avg from total # of lines and total line length
 	}
 }
