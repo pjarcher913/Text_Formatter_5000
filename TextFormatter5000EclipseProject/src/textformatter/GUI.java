5fe0059f43d426f5da6d9f1d3c840bdb02678865
Andrew Maw <admaw@asu.edu>
2018-03-15 22:21:33 -0700

    GUI.java is basically done.  Just waiting on the Formatter and Analyzer classes to completely finish it

--- TextFormatter5000EclipseProject/src/textformatter/GUI.java
+++ TextFormatter5000EclipseProject/src/textformatter/GUI.java
@@ -7,15 +7,17 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class GUI
 {
 	//These are all of the different elements of the gui.
 	private JFrame window;
 	private static JTextPane inputTextPane;
-	private JTextPane outputTextPane;
+	private static JTextPane outputTextPane;
 	private JScrollPane inputScrPane;
 	private JScrollPane outputScrPane;
 	
@@ -27,25 +29,48 @@ public class GUI
 	private JRadioButton leftJustifyButton;
 	private JRadioButton rightJustifyButton;
 	
-	private JLabel wordCountLabel;
-	private JLabel lineCountLabel;
-	private JLabel blankLinesLabel;
-	private JLabel avgWordsLabel;
-	private JLabel avgLineLabel;
+	private static JLabel wordCountLabel;
+	private static JLabel lineCountLabel;
+	private static JLabel blankLinesLabel;
+	private static JLabel avgWordsLabel;
+	private static JLabel avgLineLabel;
 	
 	private JPanel inputPanel;
 	private JPanel optionsPanel;
 	private JPanel outputPanel;
 	private JPanel analysisPanel;
 	
-	static class LoadAction implements ActionListener
+	private static int justificationFlag;
+	private static boolean fileLoaded;
+	private static boolean fileFormatted;
+	
+	public GUI()
+	{
+		justificationFlag = 0;
+		fileLoaded = false;
+		fileFormatted = false;
+	}
+	
+	private static class LoadAction implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
 		{
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
 			JFileChooser chooser = new JFileChooser();
+			chooser.setFileFilter(filter);
 			chooser.showOpenDialog(null);
 			File f = chooser.getSelectedFile();
+			if(f == null)	//No file was selected
+				return;
 			String filename = f.getAbsolutePath();
+			
+			//Double check that the file is indeed a .txt file.
+			if(!filename.endsWith("txt"))
+			{
+				//Open error box
+				JOptionPane.showMessageDialog(inputTextPane, "Please choose a file with the .txt extension.", "Error", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
 			
 			try
 			{
@@ -53,6 +78,7 @@ public class GUI
 				BufferedReader br = new BufferedReader(reader);
 				inputTextPane.read(br, null);
 				br.close();
+				fileLoaded = true;
 			}
 			catch(Exception ex)
 			{
@@ -61,7 +87,130 @@ public class GUI
 		}
 	}
 	
-	//TODO: Implement action listeners for Format, Save, and Justify buttons
+	private static class JustificationAction implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			JRadioButton button = (JRadioButton) e.getSource();
+			if(button.getText().equals("Left Justify"))
+				justificationFlag = 0;
+			else
+				justificationFlag = 1;
+		}
+	}
+	
+	private static class FormatAction implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if(!fileLoaded)
+			{
+				JOptionPane.showMessageDialog(inputTextPane, "Please open a file first!", "Not so fast!", JOptionPane.INFORMATION_MESSAGE);
+				return;
+			}
+			
+			//Build the inputArray
+			String text = inputTextPane.getText();
+			int lineCount = 1;
+			for(int i = 0; i < text.length(); i++)
+			{
+				if(text.charAt(i) == '\n')
+					lineCount++;
+			}
+			
+			String[] inputArray = new String[lineCount];
+			for(int i = 0; i < lineCount; i++)
+				inputArray[i] = "";
+			int curLocation = 0;
+			for(int i = 0; i < lineCount; i++)
+			{
+				while(curLocation < text.length() && text.charAt(curLocation) != '\n')
+				{
+					inputArray[i] += text.charAt(curLocation);
+					curLocation++;
+				}
+				++curLocation;
+			}
+			//Chop off last char of the strings
+			for(int i = 0; i < lineCount; i++)
+			{
+				if(inputArray[i].length() > 0)
+					inputArray[i] = inputArray[i].substring(0, inputArray[i].length() - 1);
+			}
+
+			//NEED OTHER FILES DONE
+			/*
+			Formatter form = new Formatter();
+			String[] outputArray = form.format(inputArray, justificationFlag);
+			Analyzer ana = new Analyzer();
+			ana.performAnalysis(inputArray, outputArray);
+			
+			wordCountLabel.setText("Word count: " + Integer.toString(ana.getWordCount()));
+			lineCountLabel.setText("Lines: " + Integer.toString(ana.getLineCount()));
+			blankLinesLabel.setText("Blank lines removed: " + Integer.toString(ana.getBlankLinesRemoved()));
+			avgWordsLabel.setText("Average words per line: " + Double.toString(ana.getAvgWordsPerLine()));
+			avgLineLabel.setText("Average line length: " + Double.toString(ana.getAvgLineLength()));
+			
+			//Build output text string
+			text = "";
+			for(int i = 0; i < outputArray.length; i++)
+			{
+				text += outputArray[i];
+				if(i != outputArray.length - 1)
+					text += "\n";
+			}
+			outputTextPane.setText(text);
+			*/
+			
+			fileFormatted = true;
+		}
+	}
+	
+	private static class SaveAction implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if(!fileLoaded)
+			{
+				JOptionPane.showMessageDialog(inputTextPane, "Please open a file first!", "Not so fast!", JOptionPane.INFORMATION_MESSAGE);
+				return;
+			}
+			else if(!fileFormatted)
+			{
+				JOptionPane.showMessageDialog(inputTextPane, "Please click the Format button first!", "Not so fast!", JOptionPane.INFORMATION_MESSAGE);
+				return;
+			}
+			
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+			JFileChooser chooser = new JFileChooser();
+			chooser.setFileFilter(filter);
+			chooser.showSaveDialog(null);
+			File f = chooser.getSelectedFile();
+			if(f == null)
+				return;
+			String filename = f.getAbsolutePath();
+			System.out.println(filename);
+			
+			//Double check that the file is indeed a .txt file.
+			if(!filename.endsWith("txt"))
+			{
+				//Open error box
+				JOptionPane.showMessageDialog(inputTextPane, "Please end the filename with the .txt extension.", "Error", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+			try
+			{
+				PrintWriter writer = new PrintWriter(filename);
+				writer.print(outputTextPane.getText());
+				writer.close();
+			}
+			catch(Exception ex)
+			{
+				JOptionPane.showMessageDialog(null, ex);
+			}
+		}
+	}
 	
 	public void createAndShowGUI()
 	{
@@ -93,11 +242,15 @@ public class GUI
 		
 		leftJustifyButton = new JRadioButton("Left Justify");
 		rightJustifyButton = new JRadioButton("Right Justify");
+		JustificationAction justListener = new JustificationAction();
+		leftJustifyButton.addActionListener(justListener);
+		rightJustifyButton.addActionListener(justListener);
 		group = new ButtonGroup();
 		group.add(leftJustifyButton);
 		group.add(rightJustifyButton);
 		leftJustifyButton.setSelected(true);
 		formatButton = new JButton("Format");
+		formatButton.addActionListener(new FormatAction());
 		optionsPanel.add(leftJustifyButton);
 		optionsPanel.add(rightJustifyButton);
 		optionsPanel.add(formatButton);
@@ -110,6 +263,7 @@ public class GUI
 		outputTextPane.setEditable(false);
 		outputTextPane.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
 		outputTextPane.setPreferredSize(new Dimension(575,150));
+		outputTextPane.setText("");
 		outputScrPane = new JScrollPane(outputTextPane, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		outputPanel.add(outputScrPane);
 		
@@ -118,6 +272,7 @@ public class GUI
 		BoxLayout analysisLayout = new BoxLayout(analysisPanel, BoxLayout.Y_AXIS);
 		analysisPanel.setLayout(analysisLayout);
 		saveButton = new JButton("Save File");
+		saveButton.addActionListener(new SaveAction());
 		wordCountLabel = new JLabel("Word count: 0");
 		lineCountLabel = new JLabel("Lines: 0");
 		blankLinesLabel = new JLabel("Blank lines removed: 0");
